6b2d93615c609ed319a5d3d5015d1df71df6a3ba
Ivobl <ivo@function.ws>
2024-07-01 21:48:29 -0300

    Add_search_fields and list_filter

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'text']
+    list_filter = ('author', 'created_date', 'published_date')
+
+admin.site.register(Post, PostAdmin)
